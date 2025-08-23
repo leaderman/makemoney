970193975f026f285006f4cdd77e9ba9788ea4a0
leaderman <miao.work2010@gmail.com>
2025-08-23 11:33:53 +0800

    common.js 增加获取多个或单个元素的方法

--- tampermonkey/common.js
+++ tampermonkey/common.js
@@ -5,8 +5,17 @@ window.mm = window.mm || {};
  * @param {string} selector 选择器。
  * @returns {string} 文本。
  */
-window.mm.getText = function (selector) {
+window.mm.text = function (selector) {
   return document.querySelector(selector)?.textContent.trim() || "";
+};
+
+/**
+ * 获取元素的文本。
+ * @param {Node} el 元素。
+ * @returns {string} 文本。
+ */
+window.mm.textOf = function (el) {
+  return el?.textContent.trim() || "";
 };
 
 /**
@@ -16,4 +25,24 @@ window.mm.getText = function (selector) {
  */
 window.mm.sleep = function (ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+/**
+ * 获取所有元素。
+ * @param {Node} root 根元素。
+ * @param {string} selector 选择器。
+ * @returns {NodeList} 元素列表。
+ */
+window.mm.all = function (root, selector) {
+  return root.querySelectorAll(selector);
+};
+
+/**
+ * 获取元素。
+ * @param {Node} root 根元素。
+ * @param {string} selector 选择器。
+ * @returns {Node} 元素。
+ */
+window.mm.one = function (root, selector) {
+  return root.querySelector(selector);
 };
--- tampermonkey/eastmoney/position.js
+++ tampermonkey/eastmoney/position.js
@@ -10,10 +10,139 @@
 (function () {
   "use strict";
 
-  console.log("Hello Position!");
+  main();
+})();
 
-  const totalAssets = window.mm.getText(
+/**
+ * 获取总资产。
+ * @returns {string} 总资产。
+ */
+function getTotalAssets() {
+  return window.mm.text(
     "#assest_cont > table > tbody > tr.tb-tr-bot.lh300 > td.tb-tr-right.pad-box > span.padl10 > span"
   );
-  console.log(totalAssets);
-})();
+}
+
+/**
+ * 获取证券市值。
+ * @returns {string} 证券市值。
+ */
+function getSecuritiesMarketValue() {
+  return window.mm.text(
+    "#assest_cont > table > tbody > tr.tb-tr-bot.lh300 > td:nth-child(2) > span.padl10 > span > span"
+  );
+}
+
+/**
+ * 获取可用资金。
+ * @returns {string} 可用资金。
+ */
+function getAvailableFunds() {
+  return window.mm.text(
+    "#assest_cont > table > tbody > tr:nth-child(2) > td.tb-tr-right.lh200.pad-box > span.padl10 > span"
+  );
+}
+
+/**
+ * 获取持仓盈亏。
+ * @returns {string} 持仓盈亏。
+ */
+function getPositionProfitLoss() {
+  return window.mm.text(
+    "#assest_cont > table > tbody > tr:nth-child(2) > td:nth-child(2) > span.padl10 > span"
+  );
+}
+
+/**
+ * 获取资金余额。
+ * @returns {string} 资金余额。
+ */
+function getCashBalance() {
+  return window.mm.text(
+    "#assest_cont > table > tbody > tr:nth-child(2) > td:nth-child(3) > span.padl10 > span"
+  );
+}
+
+/**
+ * 获取可取资金。
+ * @returns {string} 可取资金。
+ */
+function getWithdrawableFunds() {
+  return window.mm.text(
+    "#assest_cont > table > tbody > tr:nth-child(3) > td.tb-tr-right.lh200.pad-box > span.padl10 > span"
+  );
+}
+
+/**
+ * 获取当日盈亏。
+ * @returns {string} 当日盈亏。
+ */
+function getDailyProfitLoss() {
+  return window.mm.text(
+    "#assest_cont > table > tbody > tr:nth-child(3) > td:nth-child(2) > span.padl10"
+  );
+}
+
+/**
+ * 获取冻结资金。
+ * @returns {string} 冻结资金。
+ */
+function getFrozenFunds() {
+  return window.mm.text(
+    "#assest_cont > table > tbody > tr:nth-child(3) > td:nth-child(3) > span.padl10"
+  );
+}
+
+/**
+ * 等待数据。
+ */
+async function waitForData() {
+  let totalAssets = "-";
+
+  do {
+    await window.mm.sleep(100);
+
+    totalAssets = getTotalAssets();
+  } while (totalAssets === "-");
+}
+
+async function main() {
+  console.log("资金持仓脚本开始");
+
+  // 等待数据。
+  await waitForData();
+
+  // 获取总资产。
+  const totalAssets = getTotalAssets();
+  console.log("总资产：", totalAssets);
+
+  // 获取证券市值。
+  const securitiesMarketValue = getSecuritiesMarketValue();
+  console.log("证券市值：", securitiesMarketValue);
+
+  // 获取可用资金。
+  const availableFunds = getAvailableFunds();
+  console.log("可用资金：", availableFunds);
+
+  // 获取持仓盈亏。
+  const positionProfitLoss = getPositionProfitLoss();
+  console.log("持仓盈亏：", positionProfitLoss);
+
+  // 获取资金余额。
+  const cashBalance = getCashBalance();
+  console.log("资金余额：", cashBalance);
+
+  // 获取可取资金。
+  const withdrawableFunds = getWithdrawableFunds();
+  console.log("可取资金：", withdrawableFunds);
+
+  // 获取当日盈亏。
+  const dailyProfitLoss = getDailyProfitLoss();
+  console.log("当日盈亏：", dailyProfitLoss);
+
+  // 获取冻结资金。
+  const frozenFunds = getFrozenFunds();
+  console.log("冻结资金：", frozenFunds);
+
+  console.log("资金持仓脚本结束");
+}
