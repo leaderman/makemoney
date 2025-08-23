471535dda1d1616ea0f9772ab4c4b29349b6e02d
leaderman <miao.work2010@gmail.com>
2025-08-23 10:07:31 +0800

    增加 TamperMonkey 示例

---
+++ tampermonkey/hello.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Hello
+// @namespace    MakeMoney
+// @description  示例
+// @match        *://*/*
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  console.log("Hello Tampermonkey!");
+})();
