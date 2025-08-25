8683d78298f3a2d6ea4a85c706a6e9a0bf2ab101
leaderman <miao.work2010@gmail.com>
2025-08-25 16:34:55 +0800

    修复模块 eastmoney 包名错误

--- hustle/eastmoney/src/main/java/io/github/leaderman/makemoney/hustle/eastmoney/web/controller/PositionController.java
+++ hustle/eastmoney/src/main/java/io/github/leaderman/makemoney/hustle/eastmoney/web/controller/PositionController.java
@@ -1,12 +1,12 @@
-package io.github.leaderman.makemoney.eastmoney.web.controller;
+package io.github.leaderman.makemoney.hustle.eastmoney.web.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.leaderman.makemoney.eastmoney.domain.request.SyncPositionRequest;
 import io.github.leaderman.makemoney.hustle.domain.response.Response;
+import io.github.leaderman.makemoney.hustle.eastmoney.domain.request.SyncPositionRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
