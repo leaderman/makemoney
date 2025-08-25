488a3973ebbcc430add5d5f0ecfa5c9c30359dd4
leaderman <miao.work2010@gmail.com>
2025-08-25 14:09:27 +0800

    增加 web.sh

---
+++ hustle/bin/web.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 当前脚本目录
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# 配置文件目录
+CONFIG_DIR="$BASE_DIR/../../../makemoney-config"
+
+# jar 路径
+JAR_PATH="$BASE_DIR/../command/target/hustle-web.jar"
+
+# 环境
+PROFILE="${MAKEMONEY_PROFILE:-default}"
+
+# 启动参数
+SPRING_OPTS="-Dspring.config.location=file:${CONFIG_DIR}/ -Dspring.profiles.active=${PROFILE}"
+
+# 执行
+exec java ${SPRING_OPTS} -jar "${JAR_PATH}" "$@"
