c227f6c161b035db41cd18da481d310781ab4274
leaderman <miao.work2010@gmail.com>
2025-08-25 10:42:16 +0800

    增加接口 /ping

--- hustle/web/src/main/java/io/github/leaderman/makemoney/hustle/web/PingController.java
+++ hustle/web/src/main/java/io/github/leaderman/makemoney/hustle/web/PingController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello, World!";
+public class PingController {
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
   }
 }
