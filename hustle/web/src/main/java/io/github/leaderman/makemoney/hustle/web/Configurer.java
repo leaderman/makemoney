663977e7d9de5d0538a5f65a85a04cfc2882fc3c
leaderman <miao.work2010@gmail.com>
2025-09-13 19:51:15 +0800

    调试 Redis

--- hustle/web/src/main/java/io/github/leaderman/makemoney/hustle/web/Configurer.java
+++ hustle/web/src/main/java/io/github/leaderman/makemoney/hustle/web/Configurer.java
@@ -1,6 +1,9 @@
 package io.github.leaderman.makemoney.hustle.web;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,4 +29,15 @@ public class Configurer implements WebMvcConfigurer {
     String[] excludes = configClient.getStringArray("web.auth.excludes", new String[] {});
     registry.addInterceptor(authInterceptor).excludePathPatterns(excludes);
   }
+
+  @Bean
+  ApplicationRunner redisInfo(org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory f,
+      StringRedisTemplate tpl) {
+    return args -> {
+      System.out.printf("[REDIS] host=%s, port=%d%n",
+          f.getHostName(), f.getPort());
+      System.out.println("[REDIS] CF from template same? " + (tpl.getConnectionFactory() == f));
+    };
+  }
+
 }
