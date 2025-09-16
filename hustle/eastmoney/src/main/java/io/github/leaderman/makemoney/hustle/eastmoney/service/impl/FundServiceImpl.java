bc07c0c034197b9500c66cb99cfb1a2bf286ff7f
leaderman <miao.work2010@gmail.com>
2025-09-16 09:16:27 +0800

    优化价格新高和价格新低判断逻辑

--- hustle/eastmoney/src/main/java/io/github/leaderman/makemoney/hustle/eastmoney/service/impl/FundServiceImpl.java
+++ hustle/eastmoney/src/main/java/io/github/leaderman/makemoney/hustle/eastmoney/service/impl/FundServiceImpl.java
@@ -92,7 +92,8 @@ public class FundServiceImpl extends ServiceImpl<FuncMapper, FundEntity> impleme
           }
 
           // 价格近高。
-          if (NumberUtil.greaterThanOrEqualTo(entity.getHighPrice().subtract(entity.getLatestPrice()), highZone)) {
+          if (NumberUtil.greaterThan(entity.getHighPrice(), BigDecimal.ZERO)
+              && NumberUtil.greaterThanOrEqualTo(entity.getHighPrice().subtract(entity.getLatestPrice()), highZone)) {
             String title = String.format("【价格近高】%s", entity.getName());
             String content = String.format("最高价格：%s\\n最新价格：%s\\n日期时间：%s", entity.getHighPrice(),
                 entity.getLatestPrice(),
@@ -124,7 +125,8 @@ public class FundServiceImpl extends ServiceImpl<FuncMapper, FundEntity> impleme
           }
 
           // 价格近低。
-          if (NumberUtil.greaterThanOrEqualTo(entity.getLatestPrice().subtract(entity.getLowPrice()), lowZone)) {
+          if (NumberUtil.greaterThan(entity.getLowPrice(), BigDecimal.ZERO)
+              && NumberUtil.greaterThanOrEqualTo(entity.getLatestPrice().subtract(entity.getLowPrice()), lowZone)) {
             String title = String.format("【价格近低】%s", entity.getName());
             String content = String.format("最低价格：%s\\n最新价格：%s\\n日期时间：%s", entity.getLowPrice(), entity.getLatestPrice(),
                 DatetimeUtil.getDatetime());
